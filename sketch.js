246b0ab8df4582b149f5c238eabfd1133c3b0dc3
Lewis Zhang <157969244+LewisZZZ-Zhang@users.noreply.github.com>
2025-07-15 09:35:30 +0800

    Update site layout and add p5.js sketch integration
    Expanded .gitignore for broader environment and editor coverage. Updated index.html to include p5.js libraries, changed banner and sidebar images, and added a container for a p5.js sketch. Modified sketch.js to adjust canvas size and animation, and updated styles.css for improved banner image display.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,17 @@
 function setup() {
-    let cnv = createCanvas(400, 400);
+    let cnv = createCanvas(140, 400);
     cnv.parent('sketch-holder');
 }
 
 function draw() {
-    background(0);
-    fill(255);
-    circle(200, 200+200*sin(frameCount/40+PI/2), 100);
-    circle(200-200*sin(frameCount/40), 200, 100);
+    background(255, 255, 255, 50);
+    fill(0);
+    ellipseMode(CENTER);
+    // circle(200, 200+200*sin(frameCount/40+PI/2), 100);
+    r = 100;
+    circle(width/2, 
+        height/2 + (height/2-r/2) * sin(frameCount / 40 + PI / 2), 
+        r);
+    
+    // circle(200-200*sin(frameCount/40), 200, 100);
 }
